b9b0be0103c49bfc2dd0382fd9fff50573a30e41
LUCAS NEVES BELIZARIO <106932897+Belizario12@users.noreply.github.com>
2024-02-04 20:43:45 -0300

    Feat: Criando tela de geração do qrCode.

--- AcessIff/AccessIffWeb/src/app/admin/pages/qr-code/qr-code.component.ts
+++ AcessIff/AccessIffWeb/src/app/admin/pages/qr-code/qr-code.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-qr-code',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./qr-code.component.scss']
 })
 export class QrCodeComponent {
+  qrcodeForm!: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    this.qrcodeForm = this.fb.group({
+      matricula: ['', Validators.required],
+      nome: ['', Validators.required],
+      curso: ['', Validators.required],
+    })
+  }
+
+  save() {
+
+  }
+
+  back() {
+    window.history.back();
+  }
 }
