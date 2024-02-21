6f0950af9f839c8050cf5984d1966fa5ea63e574
LUCAS NEVES BELIZARIO <106932897+Belizario12@users.noreply.github.com>
2024-02-21 11:39:01 -0300

    Feat: Adicionando Url da api remota.

--- AcessIff/AccessIffWeb/src/app/utils/apiUrl.ts
+++ AcessIff/AccessIffWeb/src/app/utils/apiUrl.ts
@@ -3,5 +3,5 @@
 //export const apiUrl = 'http://localhost:5225/';
 
 //Api Dela
-export const apiUrl = 'http://http://delaflix.zapto.org:5084/';
+export const apiUrl = 'http://delaflix.zapto.org:5084/';
 
